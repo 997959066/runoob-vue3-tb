cf0b7e6c39f7ea4924b5ac19275027d483a92daa
xiaoyu <997959066@qq.com>
2025-01-12 14:33:16 +0800

    no message

---
+++ src/components/comm3.vue
@@ -0,0 +1,213 @@
+<template>
+  <div>
+    <!-- 添加 ref 标记 -->
+    <div id="container" ref="chartContainer"></div>
+  </div>
+</template>
+
+<script>
+import { onMounted, ref } from 'vue';
+import { Column } from '@antv/g2plot';
+
+
+export default {
+  setup(input) {
+    const chartContainer = ref(null); // 定义 ref
+
+    onMounted(input => {
+      // 获取正确的容器
+      const container = chartContainer.value;
+
+            const data =[
+              {
+                "city": "石家庄",
+                "type": "水果",
+                "value": 14500
+              },
+              {
+                "city": "石家庄",
+                "type": "米面",
+                "value": 8500
+              },
+              {
+                "city": "石家庄",
+                "type": "特产零食",
+                "value": 10000
+              },
+              {
+                "city": "石家庄",
+                "type": "茶叶",
+                "value": 7000
+              },
+              {
+                "city": "深圳",
+                "type": "水果",
+                "value": 9000
+              },
+              {
+                "city": "深圳",
+                "type": "米面",
+                "value": 8500
+              },
+              {
+                "city": "深圳",
+                "type": "特产零食",
+                "value": 11000
+              },
+              {
+                "city": "深圳",
+                "type": "茶叶",
+                "value": 6000
+              },
+              {
+                "city": "温州",
+                "type": "水果",
+                "value": 16000
+              },
+              {
+                "city": "温州",
+                "type": "米面",
+                "value": 5000
+              },
+              {
+                "city": "温州",
+                "type": "特产零食",
+                "value": 6000
+              },
+              {
+                "city": "温州",
+                "type": "茶叶",
+                "value": 10000
+              },
+              {
+                "city": "宁波",
+                "type": "水果",
+                "value": 14000
+              },
+              {
+                "city": "宁波",
+                "type": "米面",
+                "value": 9000
+              },
+              {
+                "city": "宁波",
+                "type": "特产零食",
+                "value": 10000
+              },
+              {
+                "city": "宁波",
+                "type": "茶叶",
+                "value": 9000
+              },
+              {
+                "city": "无锡",
+                "type": "水果",
+                "value": 14000
+              },
+              {
+                "city": "无锡",
+                "type": "米面",
+                "value": 9000
+              },
+              {
+                "city": "无锡",
+                "type": "特产零食",
+                "value": 10000
+              },
+              {
+                "city": "无锡",
+                "type": "茶叶",
+                "value": 6000
+              },
+              {
+                "city": "杭州",
+                "type": "水果",
+                "value": 9000
+              },
+              {
+                "city": "杭州",
+                "type": "米面",
+                "value": 8500
+              },
+              {
+                "city": "杭州",
+                "type": "特产零食",
+                "value": 10000
+              },
+              {
+                "city": "杭州",
+                "type": "茶叶",
+                "value": 6000
+              },
+              {
+                "city": "北京",
+                "type": "水果",
+                "value": 17000
+              },
+              {
+                "city": "北京",
+                "type": "米面",
+                "value": 6000
+              },
+              {
+                "city": "北京",
+                "type": "特产零食",
+                "value": 7000
+              },
+              {
+                "city": "北京",
+                "type": "茶叶",
+                "value": 10000
+              },
+              {
+                "city": "上海",
+                "type": "水果",
+                "value": 18000
+              },
+              {
+                "city": "上海",
+                "type": "米面",
+                "value": 11000
+              },
+              {
+                "city": "上海",
+                "type": "特产零食",
+                "value": 15000
+              },
+              {
+                "city": "上海",
+                "type": "茶叶",
+                "value": 14000
+              }
+            ];
+            const column = new Column(container, {
+              data,
+              xField: 'city',
+              yField: 'value',
+              seriesField: 'type',
+              isGroup: 'true',
+              columnStyle: {
+                radius: [20, 20, 0, 0],
+              },
+            });
+
+            column.render();
+
+
+      //onMounted-----end------
+    });
+
+    return {
+      chartContainer,
+    };
+  },
+};
+</script>
+
+<style scoped>
+/* 确保容器有固定宽高，避免无限扩展 */
+#container {
+  width: 560px; /* 宽度需与配置中的 width 保持一致 */
+  height: 376px; /* 高度需与配置中的 height 保持一致 */
+}
+</style>
--- src/views/DataPage3.vue
+++ src/views/DataPage3.vue
@@ -1,45 +1,34 @@
 <template>
   <div>
     <h1>Data 3 页面</h1>
-    <p>这里是第三个数据页面。</p>
-    <input type="text" v-model="inputValue" placeholder="输入一些内容" />
-    <button @click="submit">提交</button>
+    <div>
+      <LineChart3/>
+    </div>
     <button @click="goHome">返回首页</button>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue';
 import { useRouter } from 'vue-router';
+import LineChart3 from "@/components/comm3.vue";
 
 export default {
-  name: 'DataPage3',
+  name: 'DataPage1',
+  components: {LineChart3},
   setup() {
     const router = useRouter();
-    const inputValue = ref('');
 
     const goHome = () => {
       router.push('/'); // 返回首页
     };
 
-    const submit = () => {
-      alert(`提交的内容是：${inputValue.value}`);
-    };
-
-    return { inputValue, goHome, submit };
+    return { goHome };
   },
 };
 </script>
 
 <style scoped>
-input {
-  margin-right: 10px;
-  padding: 10px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-}
 button {
-  margin-right: 10px;
   padding: 10px 20px;
   background: #007bff;
   color: white;
